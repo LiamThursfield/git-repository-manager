c04f771807e953760774ed64b9ccd955b69b440d
Liam Thursfield <liam@DESKTOP-KSUEFOV.localdomainliam.thursfield@nexuspoint.co.uk>
2021-10-17 15:12:58 +0100

    Add pr, base, head params to the mocked pull request request

--- tests/Traits/Git/MocksGithubPullRequestWebhook.php
+++ tests/Traits/Git/MocksGithubPullRequestWebhook.php
@@ -9,14 +9,44 @@ use Illuminate\Support\Facades\Http;
 
 trait MocksGithubPullRequestWebhook
 {
+    /**
+     * Default header values
+     */
     private string $header_installation_target_type = GithubInterface::WEBHOOK_HEADER_INSTALLATION_TARGET_TYPE_REPOSITORY;
-    private string $header_event = GithubInterface::WEBHOOK_HEADER_EVENT_PULL_REQUEST;
+    private string $header_event                    = GithubInterface::WEBHOOK_HEADER_EVENT_PULL_REQUEST;
 
-    private string $repository_alias    = 'repository-name';
-    private string $repository_html_url = 'https://github.com/Username/repository-name';
-    private int|string $repository_id   = 123456789;
-    private bool $repository_private    = false;
-    private string $repository_name     = 'Username/repository-name';
+    /**
+     * Default core values
+     */
+    private string $action = GithubInterface::WEBHOOK_PULL_REQUEST_ACTION_OPENED;
+
+    /**
+     * Default base branch values
+     */
+    private string $base_ref = 'main';
+
+    /**
+     * Default head branch values
+     */
+    private string $head_ref = 'feature/branch-name';
+
+    /**
+     * Default pull request values
+     */
+    private string $pull_request_html_url   = 'https://github.com/Username/repository-name/pull/1';
+    private int|string $pull_request_id     = 987654321;
+    private string $pull_request_state      = GithubInterface::PULL_REQUEST_STATE_OPEN;
+    private string $pull_request_title      = 'A pull request title';
+
+    /**
+     * Default repository values
+     */
+    private string $repository_alias        = 'repository-name';
+    private string $repository_description  = 'A repository description';
+    private string $repository_html_url     = 'https://github.com/Username/repository-name';
+    private int|string $repository_id       = 123456789;
+    private bool $repository_private        = false;
+    private string $repository_name         = 'Username/repository-name';
 
 
     /**
@@ -35,13 +65,26 @@ trait MocksGithubPullRequestWebhook
         Http::fake([
             '*' => Http::response(
                 [
-                    'repository' => [
+                    'action'        => Arr::get($data_override, 'action', $this->action),
+                    'base'          => [
+                        'ref'   => Arr::get($data_override, 'base.ref', $this->base_ref),
+                    ],
+                    'head'          => [
+                        'ref'   => Arr::get($data_override, 'head.ref', $this->head_ref),
+                    ],
+                    'pull_request'  => [
+                        'html_url'  => Arr::get($data_override, 'pull_request.html_url', $this->pull_request_html_url),
+                        'id'        => Arr::get($data_override, 'pull_request.id', $this->pull_request_id),
+                        'state'     => Arr::get($data_override, 'pull_request.state', $this->pull_request_state),
+                        'title'     => Arr::get($data_override, 'pull_request.title', $this->pull_request_title),
+                    ],
+                    'repository'    => [
+                        'description'   => Arr::get($data_override, 'repository.description', $this->repository_description),
+                        'full_name'     => Arr::get($data_override, 'repository.name', $this->repository_name),
+                        'html_url'      => Arr::get($data_override, 'repository.html_url', $this->repository_html_url),
                         'id'            => Arr::get($data_override, 'repository.id', $this->repository_id),
                         'name'          => Arr::get($data_override, 'repository.alias', $this->repository_alias),
-                        'full_name'     => Arr::get($data_override, 'repository.name', $this->repository_name),
                         'private'       => Arr::get($data_override, 'repository.private', $this->repository_private),
-                        'html_url'      => Arr::get($data_override, 'repository.html_url', $this->repository_html_url),
-                        'description'   => 'Description of the repo',
                     ]
                 ],
                 $status_code,
