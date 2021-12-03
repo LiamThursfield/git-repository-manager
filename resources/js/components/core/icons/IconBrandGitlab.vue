0f06bcdc01cf732ede360b0ac89f907f0362ea2d
Liam Thursfield <liam@DESKTOP-KSUEFOV.localdomainliam.thursfield@nexuspoint.co.uk>
2021-12-03 13:53:38 +0000

    Add new icons - git providers - locks - sitemap

---
+++ resources/js/components/core/icons/IconBrandGitlab.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Part of the tabler icon set: https://github.com/tabler/tabler-icons -->
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z" />
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "IconBrandGitlab",
+    }
+</script>
